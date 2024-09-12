3c493e36d05658b46285cf0a40ff1a3eb97966e3
Hend Mohamed Dardir <hdardir@ejada.com>
2024-09-12 11:52:54 +0200

    clean project for e commerce app

--- src/main/java/com/example/demo/ECommerceDemoApplication.java
+++ src/main/java/com/example/demo/ECommerceDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeDemoApplication {
+public class ECommerceDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeDemoApplication.class, args);
+		SpringApplication.run(ECommerceDemoApplication.class, args);
 	}
 
 }
